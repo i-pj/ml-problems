f706b478dc869c82748bbab4c39ce139686cd2bd
Parth Jain <42parth.jain@gmail.com>
2024-07-12 23:10:24 +0530

    matrix times vector

---
+++ matrix_times_vector.py
@@ -0,0 +1,12 @@
+def matrix_dot_vector(
+    a: list[list[int | float]], b: list[int | float]
+) -> list[int | float]:
+    if not a or not b or len(a[0]) != len(b):
+        return -1
+
+    result = []
+    for row in a:
+        dot_product = sum(x * y for x, y in zip(row, b))
+        result.append(dot_product)
+
+    return result
