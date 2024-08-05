fdbe315d2775b3e858aedd6badf7357b76fa9121
Parth Jain <42parth.jain@gmail.com>
2024-08-05 13:27:13 +0530

    list comprehension

--- transpose.py
+++ transpose.py
@@ -1,12 +1,21 @@
 def transpose_matrix(a: list[list[int | float]]) -> list[list[int | float]]:
-    if not a or not a[0]:
-        return []
+    #    if not a or not a[0]:
+    #        return []
+    #
+    #    rows, cols = len(a), len(a[0])
+    #    b = [[0 for _ in range(rows)] for _ in range(cols)]
+    #
+    #    for i in range(rows):
+    #        for j in range(cols):
+    #            b[j][i] = a[i][j]
+    #
+    #    return b
 
-    rows, cols = len(a), len(a[0])
-    b = [[0 for _ in range(rows)] for _ in range(cols)]
+    # Instead of making another list b, transpose could be done
+    # like this-
 
-    for i in range(rows):
-        for j in range(cols):
-            b[j][i] = a[i][j]
+    return [list(i) for i in zip(*a)]
 
-    return b
+
+# zip(*a) does the transpose but it returns
+# a tuple.
